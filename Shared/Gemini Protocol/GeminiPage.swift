fa374fff303802e58090edabc960758550f171ea
Krueger <mrkrueger@gmail.com>
2020-08-24 18:02:42 -0700

    Added parsing of status code and meta in response header

--- Shared/Gemini Protocol/GeminiPage.swift
+++ Shared/Gemini Protocol/GeminiPage.swift
@@ -6,6 +6,25 @@ public struct GeminiPage {
     var body: String?
 
     init(response: String) {
-        body = response
+        for components in response.split(separator: "\r\n").enumerated() {
+            if components.offset == 0 {
+                for headerComponent in components.element.split(separator: " ").enumerated()  {
+                    if headerComponent.offset == 0 {
+                        if let validNumber = UInt8(headerComponent.element) {
+                            switch GeminiStatusCode(rawValue: validNumber) {
+                            case .none:
+                                break
+                            case .some(let code):
+                                statusCode = code
+                            }
+                        }
+                    } else {
+                        statusMeta = String(headerComponent.element)
+                    }
+                }
+            } else {
+                body = String(components.element)
+            }
+        }
     }
 }
